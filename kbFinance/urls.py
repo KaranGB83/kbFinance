bc939a6fcb5b48aa70d17fc4640c4d3f1376f309
KaranGB83 <bhatrekaran83@gmail.com>
2026-04-28 10:37:03 +0000

    Configured accounts with the project

--- kbFinance/urls.py
+++ kbFinance/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stocks', include("stocks.urls")),
+    path('accounts/', include("accounts.urls")),
+    path('stocks/', include("stocks.urls")),
 ]
