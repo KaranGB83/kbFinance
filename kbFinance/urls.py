95742da48bbd89e1120c527397179ae8abe8e616
KaranGB83 <bhatrekaran83@gmail.com>
2026-04-29 08:51:29 +0000

    added path for root page

--- kbFinance/urls.py
+++ kbFinance/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('stocks/', include("stocks.urls")),
+    path("", include("stocks.urls")),
 ]
